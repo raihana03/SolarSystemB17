91b604bd502877fb4f1c876fbf4609865329cac6
raihana03 <raihana.abbasi03@gmail.com>
2023-11-01 19:50:25 -0400

    this commit has the feature sun added

---
+++ src/test/java/SolarSystem/FeatureSun.java
@@ -0,0 +1,14 @@
+package SolarSystem;
+
+public class FeatureSun {
+    String name;
+    String temp;
+    String color;
+
+    //        **Feature Sun**
+    FeatureSun Sun=new FeatureSun();
+    Sun.name="panther";
+    Sun.color="black";
+    Sun.temp="-1223124kelvin";
+}
+
